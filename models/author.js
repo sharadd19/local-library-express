0605b432cba2c90c7f42898ff73d37f74d28a50f
Sharad <sharadd19@gmail.com>
2024-06-19 16:39:48 +1000

    Working library application capable of CRUD operations for books, genres, authors and book instances

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -25,6 +26,20 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+// Virtual for authors's date of birth
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth
+  ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+  : "";
+});
+
+// Virtual for authors's date of death
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death
+  ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+  : "";
 });
 
 // Export model
